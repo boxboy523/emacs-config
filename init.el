416b64148569c83e4d103dc3d81f9d0762be0b0c
Junyeong Kim <rlawnsdud523@gmail.com>
2026-01-31 02:20:42 +0900

    envrc add

--- init.el
+++ init.el
@@ -90,14 +90,14 @@
   ;; :height 120 은 12pt 크기를 의미합니다. (10배수)
   (set-face-attribute 'default nil
                       :font (font-spec :family "Terminess Nerd Font Mono"
-                                       :size 18.0
+                                       :size 14.0
                                        :weight 'medium
                                        :antialias t))
 
   ;; 2. 한글 폰트 설정 (따로 지정해야 안 깨짐)
   ;; 'hangul 스크립트에 대해서만 둥근모 Neo를 씁니다.
   (set-fontset-font t 'hangul (font-spec :name "Monoplex KR"
-                                         :size 18.0
+                                         :size 14.0
                                      	 :antialias t))
 
   ;; 3. 특수문자/이모지 설정 (선택사항)
@@ -248,6 +248,12 @@
   (marginalia-mode))
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(use-package envrc
+  :straight t
+  :demand t
+  :config
+  (envrc-global-mode))
 
 (use-package magit
   :straight t
